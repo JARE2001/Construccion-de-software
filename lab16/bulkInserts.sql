f1c88f2c86420873f7678020daf54ad2a1408cfa
Jorge <a01701350@itesm.mx>
2021-03-09 22:42:27 -0600

    lab16

--- lab16/bulkInserts.sql
+++ lab16/bulkInserts.sql
@@ -1,26 +1,37 @@
-BULK INSERT rcortese.rcortese.[Materiales]
-  FROM 'e:\wwroot\rcortese\materiales.csv'
-  WITH
-  (
-    CODEPAGE = 'ACP',
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = ' '
-  )
+BULK INSERT a1701350.a1701350.[Materiales]
+   FROM 'e:\wwwroot\a1701350\materiales.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
 
-BULK INSERT rcortese.rcortese.[Proyectos]
-  FROM 'e:\wwroot\rcortese\Proyectos.csv'
-  WITH
-  (
-    CODEPAGE = 'ACP',
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = ' '
-  )
+BULK INSERT a1701350.a1701350.[Proveedores]
+   FROM 'e:\wwwroot\a1701350\proveedores.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
 
-BULK INSERT rcortese.rcortese.[Proveedores]
-  FROM 'e:\wwroot\rcortese\Proveedores.csv'
-  WITH
-  (
-    CODEPAGE = 'ACP',
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = ' '
-  )
+BULK INSERT a1701350.a1701350.[Proyectos]
+   FROM 'e:\wwwroot\a1701350\proyectos.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+
+SET DATEFORMAT dmy
+
+BULK INSERT a1701350.a1701350.[Entregan]
+   FROM 'e:\wwwroot\a1701350\entregan.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
--- lab16/crear.sql
+++ lab16/crear.sql
@@ -1,35 +1,42 @@
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Materiales')
+  IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Materiales')
 
-DROP TABLE Materiales
+  DROP TABLE Materiales
 
-CREATE TABLE Materiales
-(
-  Clave numeric(5) not null,
-  Descripcion varchar(50),
-  Costo numeric (8,2)
-)
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Proveedores')
-DROP TABLE Proveedores
-CREATE TABLE Proveedores
-(
-  RFC char(13) not null,
-  RazonSocial varchar(50)
-)
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Proyectos')
-DROP TABLE  Proyectos
-CREATE TABLE Proyectos
-(
-  Numero numeric(5) not null,
-  Denominacion varchar(50)
-)
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Entregan')
-DROP TABLE Entregan
-CREATE TABLE Entregan
-(
-  Clave numeric(5) not null,
-  RFC char(13) not null,
-  Numero numeric(5) not null,
-  Fecha DateTime not null,
-  Cantidad numeric (8,2)
-)
+  CREATE TABLE Materiales
+  (
+    Clave numeric(5) not null,
+    Descripcion varchar(50),
+    Costo numeric (8,2),
+    PRIMARY KEY(Clave)
+  )
+  IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Proveedores')
+  DROP TABLE Proveedores
+  CREATE TABLE Proveedores
+  (
+    RFC char(13) not null,
+    RazonSocial varchar(50),
+    PRIMARY KEY(RFC)
+  )
+  IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Proyectos')
+  DROP TABLE  Proyectos
+  CREATE TABLE Proyectos
+  (
+    Numero numeric(5) not null,
+    Denominacion varchar(50),
+    PRIMARY KEY(Numero)
+  )
+  IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Entregan')
+  DROP TABLE Entregan
+  CREATE TABLE Entregan
+  (
+    Clave numeric(5) not null,
+    RFC char(13) not null,
+    Numero numeric(5) not null,
+    Fecha DateTime not null,
+    Cantidad numeric (8,2),
+    PRIMARY KEY (Clave,RFC,Numero),
+    FOREIGN KEY (Clave) REFERENCES Materiales(Clave),
+    FOREIGN KEY (RFC) REFERENCES Proveedores(RFC),
+    FOREIGN KEY (Numero) REFERENCES Proyectos(Numero)
+  )
 
